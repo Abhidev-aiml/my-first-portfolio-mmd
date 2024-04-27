03318d7971556ff685afa0443f95848053ba645e
Abhidev-aiml <abhishekyadav25193@gmail.com>
2024-04-27 12:23:36 +0530

    Final Site

---
+++ src/components/Header.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { createClient } from "@/prismicio";
+import Link from "next/link";
+import { PrismicNextLink } from "@prismicio/next";
+import NavBar from "@/components/Navbar";
+
+export default async function Header() {
+    const client = createClient();
+    const settings  = await client.getSingle("settings");
+    return (
+        <header className="top-0 z-50 mx-auto max-w-7x1 md:sticky md:top-4">
+            <NavBar settings={settings} />
+        </header>
+
+
+    ); 
+}
